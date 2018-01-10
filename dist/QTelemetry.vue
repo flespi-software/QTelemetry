03712ab9f2e99f93abfd899d3ff3af5559933dcc
Sergei Buntsevich <sebu@gurtam.com>
2018-01-10 11:12:48 +0300

    [feature] copy for value in telemetry

--- dist/QTelemetry.vue
+++ dist/QTelemetry.vue
@@ -10,7 +10,13 @@
         <q-item @click="clickItemHandler(index, key)" v-if="Object.keys(filteredTelemetry).length" v-for="(key, index) in Object.keys(filteredTelemetry)" :key="key" style="transition: all .5s ease-in-out" :class="[!prevTelemetry[key] || prevTelemetry[key].value !== telemetry[key].value ? cls.highlight : cls.bg, cls.bg]">
             <q-item-main>
                 <q-item-tile label class="ellipsis text-bold"  :class="[cls.text]">{{key}}<q-tooltip>{{key}}</q-tooltip></q-item-tile>
-                <q-item-tile sublabel class="ellipsis"  :class="[cls.text]">{{telemetry[key].value}}<q-tooltip>{{telemetry[key].value}}</q-tooltip></q-item-tile>
+                <q-item-tile sublabel class="ellipsis"  :class="[cls.text]">
+                    <q-icon style="padding-right: 1px" v-if="!!$copyText" name="mdi-content-copy" @click.stop="copyMessageHandler({index, content: telemetry[key].value})"><q-tooltip>copy</q-tooltip></q-icon>
+                    <span>
+                        {{telemetry[key].value}}
+                        <q-tooltip>{{telemetry[key].value}}</q-tooltip>
+                    </span>
+                </q-item-tile>
             </q-item-main>
             <q-item-side right><small  :class="[cls.text]">{{fromNow(telemetry[key].ts * 1000)}}</small><q-tooltip><small>{{getTime(telemetry[key].ts * 1000)}}</small></q-tooltip></q-item-side>
             <q-popover class="shadow-1" ref="popovers">
@@ -34,9 +40,14 @@
 </template>
 
 <script>
-    import { QList, QListHeader, QItem, QItemMain, QItemSide, QItemTile, QTooltip, QIcon, QPopover } from 'quasar-framework'
+    import { QList, QListHeader, QItem, QItemMain, QItemSide, QItemTile, QTooltip, QIcon, QPopover, Toast } from 'quasar-framework'
+    import Vue from 'vue'
     import { createNamespacedHelpers } from 'vuex'
     import moment from 'moment'
+    import VueClipboard from 'vue-clipboard2'
+    import 'mdi/css/materialdesignicons.min.css'
+
+    Vue.use(VueClipboard)
 
     let { mapState, mapActions, mapMutations } = createNamespacedHelpers('telemetry')
 
@@ -141,6 +152,21 @@
                     }
                     return acc
                 }, {})
+            },
+            copyMessageHandler ({index, content}) {
+                this.$copyText(JSON.stringify(content)).then(function (e) {
+                    Toast.create.positive({
+                        icon: 'content_copy',
+                        html: `Value copied`,
+                        timeout: 1000
+                    })
+                }, function (e) {
+                    Toast.create.negative({
+                        icon: 'content_copy',
+                        html: `Error coping value`,
+                        timeout: 1000
+                    })
+                })
             }
         },
         components: { QList, QListHeader, QItem, QItemMain, QItemSide, QItemTile, QTooltip, QIcon, QPopover },
