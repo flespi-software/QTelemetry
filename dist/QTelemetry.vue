fe564c5142459687d51fcfd24189724fa0a01f35
scarry1992 <scarry92@yandex.ru>
2019-09-18 16:25:58 +0300

    [update] optimizations

--- dist/QTelemetry.vue
+++ dist/QTelemetry.vue
@@ -1,232 +1,300 @@
 <template>
-    <q-list separator no-border class="relative-position no-padding" style="min-height: 30vh">
-        <q-item v-if="(!Object.keys(telemetry).length || !Object.keys(filteredTelemetry).length) && !isLoading" :class="[cls.bg]">
-            <q-item-main>
-                <q-item-tile label class="ellipsis text-bold text-center" :class="[cls.text]">Telemetry is empty</q-item-tile>
-                <q-item-tile v-if="!Object.keys(telemetry).length" sublabel class="ellipsis text-center" :class="[cls.text]">Init your device</q-item-tile>
-                <q-item-tile v-if="!Object.keys(filteredTelemetry).length && this.search" sublabel class="ellipsis text-center" :class="[cls.text]">Nothing found on your search</q-item-tile>
-            </q-item-main>
-        </q-item>
-        <div v-if="isLoading" style="text-align: center; margin-top: 10%;">
-            <q-spinner-gears size="70px" color="white" />
-        </div>
-        <template v-if="Object.keys(filteredTelemetry).length && !isLoading">
-            <q-item @click="clickItemHandler(index, key)" v-for="(key, index) in Object.keys(filteredTelemetry)" :key="key" style="transition: all .5s ease-in-out" :class="[!prevTelemetry[key] || prevTelemetry[key].value !== telemetry[key].value ? cls.highlight : cls.bg]">
-                <q-item-main>
-                    <q-item-tile label class="ellipsis text-bold"  :class="[cls.text]">{{key}}<q-tooltip>{{key}}</q-tooltip></q-item-tile>
-                    <q-item-tile sublabel class="ellipsis"  :class="[cls.text]">
-                        <q-icon style="padding-right: 1px; cursor: pointer;" v-if="!!$copyText" name="mdi-content-copy" @click.stop.native="copyMessageHandler({index, content: telemetry[key].value})"><q-tooltip>copy</q-tooltip></q-icon>
-                        <span>
-                            {{telemetry[key].value}}
-                            <q-tooltip>{{telemetry[key].value}}</q-tooltip>
-                        </span>
-                    </q-item-tile>
-                </q-item-main>
-                <q-item-side right><small  :class="[cls.text]">{{fromNow(telemetry[key].ts * 1000)}}</small><q-tooltip><small>{{getTime(telemetry[key].ts * 1000)}}</small></q-tooltip></q-item-side>
-                <q-popover class="shadow-1" ref="popovers">
-                    <q-list v-if="propHistoryFlag && history[key] && history[key].length" separator no-border>
-                        <q-item>
-                            <q-item-side left><q-icon size="1.5rem" name="history"/></q-item-side>
-                            <q-item-main>
-                                <q-item-tile class="ellipsis">History</q-item-tile>
-                            </q-item-main>
-                        </q-item>
-                        <q-item v-for="(obj, index) in history[key]" :key="index">
-                            <q-item-main>
-                                <q-item-tile label class="ellipsis">{{obj.value}}</q-item-tile>
-                            </q-item-main>
-                            <q-item-side right><small>{{getTime(obj.ts * 1000)}}</small></q-item-side>
-                        </q-item>
-                    </q-list>
-                </q-popover>
+  <q-list separator no-border class="relative-position no-padding" style="min-height: 30vh">
+    <q-item v-if="(!telemetryKeys.length || !filteredTelemetryKeys.length) && !isLoading" :class="[cls.bg]">
+      <q-item-main>
+        <q-item-tile label class="ellipsis text-bold text-center" :class="[cls.text]">Telemetry is empty</q-item-tile>
+        <q-item-tile v-if="!telemetryKeys.length" sublabel class="ellipsis text-center" :class="[cls.text]">Init your
+          device</q-item-tile>
+        <q-item-tile v-if="!filteredTelemetryKeys.length && this.search" sublabel class="ellipsis text-center"
+          :class="[cls.text]">Nothing found on your search</q-item-tile>
+      </q-item-main>
+    </q-item>
+    <div v-if="isLoading" style="text-align: center; margin-top: 10%;">
+      <q-spinner-gears size="70px" color="white" />
+    </div>
+    <VirtualList
+      :size="55"
+      :remain="15"
+      v-if="filteredTelemetryKeys.length && !isLoading"
+      style="position: absolute; top: 0; bottom: 0; left: 0; right: 0; height: auto;"
+    >
+      <q-item
+        @click="clickItemHandler(index, key)"
+        v-for="(valueObj, key, index) in filteredTelemetry"
+        :key="key"
+        style="transition: all .5s ease-in-out"
+        :class="[!prevTelemetry[key] || prevTelemetry[key].value !== telemetry[key].value ? cls.highlight : cls.bg]"
+      >
+        <q-item-main>
+          <q-item-tile label class="ellipsis text-bold" :class="[cls.text]">{{key}}<q-tooltip>{{key}}</q-tooltip>
+          </q-item-tile>
+          <q-item-tile sublabel class="ellipsis" :class="[cls.text]">
+            <q-icon style="padding-right: 1px; cursor: pointer;" v-if="!!$copyText" name="mdi-content-copy"
+              @click.stop.native="copyMessageHandler({index, content: telemetry[key].value})">
+              <q-tooltip>copy</q-tooltip>
+            </q-icon>
+            <span>
+              {{telemetry[key].value}}
+              <q-tooltip>{{telemetry[key].value}}</q-tooltip>
+            </span>
+          </q-item-tile>
+        </q-item-main>
+        <q-item-side right><small :class="[cls.text]">{{fromNow(telemetry[key].ts * 1000)}}</small>
+          <q-tooltip><small>{{getTime(telemetry[key].ts * 1000)}}</small></q-tooltip>
+        </q-item-side>
+        <q-popover class="shadow-1" ref="popovers">
+          <q-list v-if="propHistoryFlag && history[key] && history[key].length" separator no-border>
+            <q-item>
+              <q-item-side left>
+                <q-icon size="1.5rem" name="history" />
+              </q-item-side>
+              <q-item-main>
+                <q-item-tile class="ellipsis">History</q-item-tile>
+              </q-item-main>
             </q-item>
-        </template>
-    </q-list>
+            <q-item v-for="(obj, index) in history[key]" :key="index">
+              <q-item-main>
+                <q-item-tile label class="ellipsis">{{obj.value}}</q-item-tile>
+              </q-item-main>
+              <q-item-side right><small>{{getTime(obj.ts * 1000)}}</small></q-item-side>
+            </q-item>
+          </q-list>
+        </q-popover>
+      </q-item>
+    </VirtualList>
+  </q-list>
 </template>
 
 <script>
-    import telemetryVuexModule from './telemetryVuexModule'
-    import Vue from 'vue'
-    import { mapState } from 'vuex'
-    import moment from 'moment'
-    import VueClipboard from 'vue-clipboard2'
+import telemetryVuexModule from './telemetryVuexModule'
+import Vue from 'vue'
+import {
+  mapState
+} from 'vuex'
+import moment from 'moment'
+import VueClipboard from 'vue-clipboard2'
+import debounce from 'lodash/debounce'
+import VirtualList from 'vue-virtual-scroll-list'
 
-    Vue.use(VueClipboard)
+Vue.use(VueClipboard)
 
-    export default {
-        name: 'QTelemetry',
-        props: {
-            device: {
-                type: Object,
-                required: true
-            },
-            moduleName: {
-                type: String,
-                default: 'telemetry'
-            },
-            propHistoryFlag: {
-                type: Boolean,
-                default: false
-            },
-            search: {
-                type: String,
-                default: ''
-            },
-            color: {
-                type: String,
-                default: ''
-            },
-            inverted: Boolean
-        },
-        data () {
-            return {
-                prevTelemetry: { ...this.device.telemetry },
-                history: {}
-            }
-        },
-        computed: {
-            ...mapState({
-                deviceId (state) {
-                    return this.moduleName && state[this.moduleName] ? state[this.moduleName].deviceId : null
-                },
-                telemetry (state) {
-                    return this.moduleName && state[this.moduleName] ? state[this.moduleName].telemetry : {}
-                },
-                isLoading (state) {
-                    return this.moduleName && state[this.moduleName] ? state[this.moduleName].isLoading : false
-                }
-            }),
-            cls () {
-                const cls = {
-                    text: `text-${this.color}`,
-                    bg: '',
-                    highlight: 'bg-grey-3'
-                }
-                if (this.inverted) {
-                    if (this.color) {
-                        cls.text = `text-${this.color}`
-                    } else {
-                        cls.text = 'text-white'
-                    }
-                    cls.bg = `bg-dark`
-                    cls.highlight = `bg-grey-7`
-                }
-                return cls
-            },
-            filteredTelemetry () {
-                return Object.keys(this.telemetry).reduce((acc, key) => {
-                    if (key.indexOf(this.search) !== -1) {
-                        acc[key] = this.telemetry[key]
-                    }
-                    return acc
-                }, {})
-            }
-        },
-        methods: {
-            init (payload) {
-                this.$store.commit(`${this.moduleName}/init`, payload)
-            },
-            clear () {
-                this.$store.commit(`${this.moduleName}/clear`)
-            },
-            update () {
-                return this.$store.dispatch(`${this.moduleName}/update`)
-            },
-            unsubscribe () {
-                return this.$store.dispatch(`${this.moduleName}/unsubscribe`)
-            },
-            fromNow (ts) {
-                return moment(ts).fromNow()
-            },
-            getTime (ts) {
-                return moment(ts).format('L HH:mm:ss')
-            },
-            clickItemHandler (index, key) {
-                this.$emit('click:item', { deviceId: this.deviceId })
-            },
-            makeHistory (newTelemetry) {
-                Object.keys(newTelemetry).forEach(key => {
-                    if (!this.history[key]) {
-                        this.history[key] = [newTelemetry[key]]
-                    }
-                    if (this.history[key].length) {
-                        if (this.history[key][0].value !== newTelemetry[key].value) {
-                            this.history[key].unshift(newTelemetry[key])
-                            if (this.history[key].length > 10) {
-                                this.history[key].pop()
-                            }
-                        }
-                    }
-                })
-            },
-            getDiff () {
-                return Object.keys(this.telemetry).reduce((acc, key) => {
-                    if (!this.prevTelemetry[key] || this.prevTelemetry[key].value !== this.telemetry[key].value) {
-                        acc[key] = this.telemetry[key]
-                    }
-                    return acc
-                }, {})
-            },
-            copyMessageHandler ({ index, content }) {
-                this.$copyText(JSON.stringify(content))
-                    .then((e) => {
-                        this.$q.notify({
-                            type: 'positive',
-                            icon: 'content_copy',
-                            message: `Value copied`,
-                            timeout: 1000,
-                            position: 'bottom-left'
-                        })
-                    }, (e) => {
-                        this.$q.notify({
-                            type: 'negative',
-                            icon: 'content_copy',
-                            message: `Error coping value`,
-                            timeout: 1000,
-                            position: 'bottom-left'
-                        })
-                    })
-            }
-        },
-        watch: {
-            device (device) {
-                if (device.id) {
-                    if (device.id !== this.deviceId) {
-                        this.unsubscribe()
-                        this.init(device)
-                        this.update()
-                        this.history = {}
-                        this.prevTelemetry = { ...this.device.telemetry }
-                    }
-                } else {
-                    this.clear()
-                    this.history = {}
-                    this.prevTelemetry = {}
-                }
-            },
-            telemetry: {
-                deep: true,
-                handler (telemetry) {
-                    const diff = this.getDiff()
-                    Object.keys(diff).length ? this.$emit('diff', diff) : this.$emit('diff', { ...telemetry })
-                    setTimeout(() => {
-                        this.prevTelemetry = { ...telemetry }
-                        if (this.propHistoryFlag) {
-                            this.makeHistory(telemetry)
-                        }
-                    }, 1000)
-                }
-            }
-        },
-        created () {
-            this.$store.registerModule(this.moduleName, telemetryVuexModule(this.$store, Vue))
-            this.init(this.device)
-            this.update()
-        },
-        beforeDestroy () {
-            this.clear()
-        }
+export default {
+  name: 'QTelemetry',
+  props: {
+    device: {
+      type: Object,
+      required: true
+    },
+    moduleName: {
+      type: String,
+      default: 'telemetry'
+    },
+    propHistoryFlag: {
+      type: Boolean,
+      default: false
+    },
+    search: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: ''
+    },
+    inverted: Boolean
+  },
+  data() {
+    return {
+      prevTelemetry: {
+        ...this.device.telemetry
+      },
+      history: {},
+      filteredTelemetry: {},
+      prevTelemetryTimeout: 0
     }
+  },
+  computed: {
+    ...mapState({
+      deviceId(state) {
+        return this.moduleName && state[this.moduleName] ? state[this.moduleName].deviceId : null
+      },
+      telemetry(state) {
+        return this.moduleName && state[this.moduleName] ? state[this.moduleName].telemetry : {}
+      },
+      isLoading(state) {
+        return this.moduleName && state[this.moduleName] ? state[this.moduleName].isLoading : false
+      }
+    }),
+    cls() {
+      const cls = {
+        text: `text-${this.color}`,
+        bg: '',
+        highlight: 'bg-grey-3'
+      }
+      if (this.inverted) {
+        if (this.color) {
+          cls.text = `text-${this.color}`
+        } else {
+          cls.text = 'text-white'
+        }
+        cls.bg = `bg-dark`
+        cls.highlight = `bg-grey-7`
+      }
+      return cls
+    },
+    telemetryKeys() {
+      return Object.keys(this.telemetry)
+    },
+    filteredTelemetryKeys() {
+      return Object.keys(this.filteredTelemetry)
+    }
+  },
+  methods: {
+    init(payload) {
+      this.$store.commit(`${this.moduleName}/init`, payload)
+    },
+    clear() {
+      this.$store.commit(`${this.moduleName}/clear`)
+    },
+    update() {
+      return this.$store.dispatch(`${this.moduleName}/update`)
+    },
+    unsubscribe() {
+      return this.$store.dispatch(`${this.moduleName}/unsubscribe`)
+    },
+    fromNow(ts) {
+      return moment(ts).fromNow()
+    },
+    getTime(ts) {
+      return moment(ts).format('L HH:mm:ss')
+    },
+    clickItemHandler(index, key) {
+      this.$emit('click:item', {
+        deviceId: this.deviceId
+      })
+    },
+    makeHistory(newTelemetry) {
+      Object.keys(newTelemetry).forEach(key => {
+        if (!this.history[key]) {
+          this.history[key] = [newTelemetry[key]]
+        }
+        if (this.history[key].length) {
+          if (this.history[key][0].value !== newTelemetry[key].value) {
+            this.history[key].unshift(newTelemetry[key])
+            if (this.history[key].length > 10) {
+              this.history[key].pop()
+            }
+          }
+        }
+      })
+    },
+    getDiff() {
+      return Object.keys(this.telemetry).reduce((acc, key) => {
+        if (!this.prevTelemetry[key] || this.prevTelemetry[key].value !== this.telemetry[key].value) {
+          acc[key] = this.telemetry[key]
+        }
+        return acc
+      }, {})
+    },
+    copyMessageHandler({
+      index,
+      content
+    }) {
+      this.$copyText(JSON.stringify(content))
+        .then((e) => {
+          this.$q.notify({
+            type: 'positive',
+            icon: 'content_copy',
+            message: `Value copied`,
+            timeout: 1000,
+            position: 'bottom-left'
+          })
+        }, (e) => {
+          this.$q.notify({
+            type: 'negative',
+            icon: 'content_copy',
+            message: `Error coping value`,
+            timeout: 1000,
+            position: 'bottom-left'
+          })
+        })
+    },
+    setFilteredTelemetry(telemetry) {
+      let filteredTelemetry = this.filteredTelemetry
+      this.telemetryKeys.forEach((key) => {
+        if (key.indexOf(this.search) !== -1) {
+          this.$set(filteredTelemetry, key, this.telemetry[key])
+        } else if (filteredTelemetry[key]) {
+          this.$delete(filteredTelemetry, key)
+        }
+      })
+    },
+    telemetryDiffHistoryProcessing(telemetry) {
+      const diff = this.getDiff()
+      Object.keys(diff).length ? this.$emit('diff', diff) : this.$emit('diff', {
+        ...telemetry
+      })
+      if (this.prevTelemetryTimeout) {
+        clearTimeout(this.prevTelemetryTimeout)
+      }
+      this.prevTelemetryTimeout = setTimeout(() => {
+        this.prevTelemetry = {
+          ...telemetry
+        }
+        if (this.propHistoryFlag) {
+          this.makeHistory(telemetry)
+        }
+        this.prevTelemetryTimeout = 0
+      }, 1000)
+    },
+    telemetryProcessing(telemetry) {
+      this.setFilteredTelemetry(telemetry)
+      this.telemetryDiffHistoryProcessing(telemetry)
+    }
+  },
+  watch: {
+    device(device) {
+      if (device.id) {
+        if (device.id !== this.deviceId) {
+          this.unsubscribe()
+          this.init(device)
+          this.update()
+          this.history = {}
+          this.prevTelemetry = {
+            ...this.device.telemetry
+          }
+        }
+      } else {
+        this.clear()
+        this.history = {}
+        this.prevTelemetry = {}
+      }
+    },
+    telemetry: {
+      deep: true,
+      handler(telemetry) {
+        this.debouncedTelemetryProcessing(telemetry)
+      }
+    },
+    search() {
+      this.setFilteredTelemetry(this.telemetry)
+    }
+  },
+  created() {
+    this.debouncedTelemetryProcessing = debounce(this.telemetryProcessing, 500, {
+      trailing: true
+    })
+    this.$store.registerModule(this.moduleName, telemetryVuexModule(this.$store, Vue))
+    this.init(this.device)
+    this.update()
+  },
+  beforeDestroy() {
+    this.clear()
+  },
+  components: {
+    VirtualList
+  }
+}
 </script>
 
 <style>
--- src/telemetryVuexModule.js
+++ src/telemetryVuexModule.js
@@ -1,68 +1,90 @@
 export default function (Store, Vue) {
-    const state = {
-        deviceId: null,
-        telemetry: {},
-        isLoading: false
-    }
+  const state = {
+    deviceId: null,
+    telemetry: {},
+    isLoading: false,
+    updateIntervalId: 0
+  }
 
-    const actions = {
-        update: async function ({ state, commit }) {
-            try {
-                /* init telemetry */
-                if (state.deviceId && !Object.keys(state.telemetry).length) {
-                    state.isLoading = true
-                    /* subscribe to new device telemetry */
-                    let grants = await Vue.connector.socket.subscribe({
-                        name: `flespi/state/gw/devices/${state.deviceId}/telemetry/+`,
-                        handler (message, topic, packet) {
-                            let name = topic.split('/').reverse()[0]
-                            if (name === 'position') { return false }
-                            let value = message.toString(),
-                                ts = packet.properties && packet.properties.userProperties && packet.properties.userProperties.timestamp,
-                                telemetry = {
-                                    [name]: {
-                                        value,
-                                        ts
-                                    }
-                                }
-                            commit('setParameter', telemetry)
-                        }
-                    })
-                    state.isLoading = false
+  const actions = {
+    update: async function ({
+      state,
+      commit
+    }) {
+      try {
+        /* init telemetry */
+        if (state.deviceId && !Object.keys(state.telemetry).length) {
+          state.isLoading = true
+          let messages = [],
+            messagesProcess = () => {
+              messages.forEach((packet) => {
+                let topic = packet.topic,
+                  message = packet.payload,
+                  name = topic.split('/').reverse()[0]
+                if (name === 'position') {
+                  return false
                 }
-            } catch (error) {
-                commit('reqFailed', error, { root: true })
+                let value = message.toString(),
+                  ts = packet.properties && packet.properties.userProperties && packet.properties.userProperties.timestamp,
+                  telemetry = {
+                    [name]: {
+                      value,
+                      ts
+                    }
+                  }
+                commit('setParameter', telemetry)
+              })
+              messages = []
             }
-        },
-        unsubscribe: async function ({ state, commit }) {
-            Vue.connector.socket.unsubscribe(`flespi/state/gw/devices/${state.deviceId}/telemetry/+`)
+          /* subscribe to new device telemetry */
+          let grants = await Vue.connector.socket.subscribe({
+            name: `flespi/state/gw/devices/${state.deviceId}/telemetry/+`,
+            handler(message, topic, packet) {
+              messages.push(packet)
+            }
+          })
+          state.updateIntervalId = setInterval(messagesProcess, 500)
+          state.isLoading = false
         }
+      } catch (error) {
+        commit('reqFailed', error, {
+          root: true
+        })
+      }
+    },
+    unsubscribe: async function ({
+      state,
+      commit
+    }) {
+      clearInterval(state.updateIntervalId)
+      Vue.connector.socket.unsubscribe(`flespi/state/gw/devices/${state.deviceId}/telemetry/+`)
     }
+  }
 
-    const mutations = {
-        init (state, device) {
-            Vue.set(state, 'deviceId', device.id || 0)
-            Vue.set(state, 'telemetry', device.telemetry || {})
-        },
+  const mutations = {
+    init(state, device) {
+      Vue.set(state, 'deviceId', device.id || 0)
+      Vue.set(state, 'telemetry', device.telemetry || {})
+    },
 
-        setParameter (state, payload) {
-            Object.keys(payload).forEach(key => {
-                if (!state.telemetry[key] || (state.telemetry[key] && state.telemetry[key].ts <= payload[key].ts)) {
-                    Vue.set(state.telemetry, key, payload[key])
-                }
-            })
-        },
-        clear (state) {
-            Vue.connector.socket.unsubscribe(`flespi/state/gw/devices/${state.deviceId}/telemetry/+`)
-            Vue.set(state, 'telemetry', {})
-            Vue.set(state, 'deviceId', null)
+    setParameter(state, payload) {
+      Object.keys(payload).forEach(key => {
+        if (!state.telemetry[key] || (state.telemetry[key] && state.telemetry[key].ts <= payload[key].ts)) {
+          Vue.set(state.telemetry, key, payload[key])
         }
+      })
+    },
+    clear(state) {
+      Vue.connector.socket.unsubscribe(`flespi/state/gw/devices/${state.deviceId}/telemetry/+`)
+      Vue.set(state, 'telemetry', {})
+      Vue.set(state, 'deviceId', null)
     }
+  }
 
-    return {
-        namespaced: true,
-        state,
-        actions,
-        mutations
-    }
+  return {
+    namespaced: true,
+    state,
+    actions,
+    mutations
+  }
 }
